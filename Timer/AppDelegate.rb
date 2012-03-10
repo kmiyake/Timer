086a038a757975e6efe4633b420107bf4dcba8a0
Kota Miyake <miyake.kota@gmail.com>
2012-03-11 07:09:27 +0900

    first commit

--- Timer/AppDelegate.rb
+++ Timer/AppDelegate.rb
@@ -8,8 +8,42 @@
 
 class AppDelegate
     attr_accessor :window
+    attr_accessor :timeLabel, :currentTime
     def applicationDidFinishLaunching(a_notification)
         # Insert code here to initialize your application
+    end
+    
+    
+    
+    def startTimer(sender)
+        if @thread.nil?
+            @thread = Thread.start do
+                @currentTime = 25 * 60
+                loop do
+                    stopTimer if @currentTime < 1
+                    timeLabel.stringValue = currentTimeAsString
+                    sleep 1
+                    @currentTime -= 1
+                end
+            end
+        end
+    end
+    
+    def stopTimer(sender)
+        if @thread
+            @thread.kill
+            @thread = nil
+        end
+    end
+    
+    def currentTimeAsString
+        p currentTime
+        minutes = (currentTime % 3600) / 60
+        seconds = (currentTime % 60)
+        minutes = 60 if currentTime == 3600
+        m = sprintf("%02i", minutes)
+        s = sprintf("%02i", seconds)
+        "#{m}:#{s}"
     end
 end
 
